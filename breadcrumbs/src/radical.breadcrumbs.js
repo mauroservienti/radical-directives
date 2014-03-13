b5df8a8dfa5fa1c5bd4a7cab376c3db1ce949796
Mauro Servienti <mauro@topics.it>
2014-03-13 08:17:42 +0100

    adding samples, changed the config to a provider, added support hashPrefix and html5mode

--- breadcrumbs/src/radical.breadcrumbs.js
+++ breadcrumbs/src/radical.breadcrumbs.js
@@ -2,15 +2,31 @@
 
 	var module = angular.module('radical.breadcrumbs', ['ui.router']);
 
-	module.factory('breadcrumbsConfig', [function(){ 
-        return {
-            templateUrl: '/radical/directives/breadcrumbs/template.html',
-            defaultItemNameResolver: function(state) {
-            	var displayName = state.data.settings.displayName || state.name;
-                return displayName;
-            }
+    module.provider('breadcrumbsConfig', function breadcrumbsConfigProvider() {
+        this.templateUrl = '/radical/directives/breadcrumbs/template.html',
+        this.defaultItemNameResolver = function(state, stateParams, isCurrent) {
+            var displayName = state.data.settings.displayName || state.name;
+            return displayName;
         };
-    }]);
+        hashPrefix = '';
+        html5Mode = true;
+     
+      this.$get = function breadcrumbsConfigFactory() {
+         return this;
+      };
+    });
+
+	// module.factory('breadcrumbsConfig', [function(){ 
+ //        return {
+ //            templateUrl: '/radical/directives/breadcrumbs/template.html',
+ //            defaultItemNameResolver: function(state) {
+ //            	var displayName = state.data.settings.displayName || state.name;
+ //                return displayName;
+ //            },
+ //            hashPrefix:'',
+ //            html5Mode: true
+ //        };
+ //    }]);
 
 	module.directive('breadcrumbs', ['$log', 'breadcrumbsConfig','$parse', function ($log, config, $parse) {
         return {
@@ -36,15 +52,30 @@
                                 return $scope.displayNameResolver({
                                     defaultResolver: config.defaultItemNameResolver,
                                     state: state,
+                                    stateParams: $stateParams,
                                     isCurrent: isCurrent(state)
                                 });
                             }
                             else {
-                                return config.defaultItemNameResolver(state);
+                                return config.defaultItemNameResolver(state, $stateParams, isCurrent(state));
                             }
                         },
                         isCurrent: function (state) {
                             return isCurrent(state);
+                        },
+                        formatUrl: function(state){
+
+                            var url = state.url.format($stateParams);
+
+                            if(config.hashPrefix){
+                                url = config.hashPrefix + url;
+                            }
+
+                            if(!config.html5Mode){
+                                url = '#'+ url;
+                            }
+
+                            return url;
                         }
                     }
                 };
